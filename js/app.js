e2f539e72054768fee87900aece0178206017aa4
dora5n <Moe4dora5n@gmail.com>
2023-06-15 19:08:23 +0900

    js配列の練習

--- js/app.js
+++ js/app.js
@@ -1,6 +1,37 @@
 // alert(1111);
 
 
-
 //jQueryを使うときのルール htmlに目印をつけること！
 // $("").on("click", function () {});
+
+
+let rate = 0.8;
+console.log("前砂:" + 80 * rate + "点");
+console.log("おか:" + 90 * rate + "点");
+console.log("わた:" + 70 * rate + "点");
+
+let animals = ["dog", "cat", "lion",];
+console.log(animals[1]);
+animals[1] = "tiger";
+console.log(animals[1]);
+
+let data = [
+  ["太郎", "24歳", "180cm", "65kg"],
+  ["次郎", "21歳", "176cm", "63kg"],
+  ["三郎", "19歳", "198cm", "89kg"]
+];
+console.log(data[1][2]);
+
+let kazu = [
+  [0,1,2],
+  [3,,5]
+];
+console.log(kazu[1][1]);
+kazu[1][1] = "4";
+console.log(kazu[1][1]);
+
+
+
+
+
+
